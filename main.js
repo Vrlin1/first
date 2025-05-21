8ddc7641da321b6466b695f779bc78f4df31874e
eva <>
2025-05-21 11:38:32 +0900

    update

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ const qOne = document.querySelector('.q_one')
 const qTwo = document.querySelector('.q_two')
 const qThree = document.querySelector('.q_three')
 const qFour = document.querySelector('.q_four')
+const bStart = document.querySelector('.start')
+const bStop = document.querySelector('.stop')
 
+let bpm = 20
+
+let myAudio = document.querySelector('.audio')
+let rangeUser = document.querySelector('.range')
+let playFunction
 
 function addCircle(count) {
     document.querySelector('.circles').innerHTML = ''
@@ -13,11 +20,34 @@ function addCircle(count) {
         `
     }
 }
+
+
 //Вызов функции при нажатии на кнопки
-qOne.addEventListener('click', addCircle(1))
-qTwo.addEventListener('click', addCircle(2))
-qThree.addEventListener('click', addCircle(3))
-//
+qOne.addEventListener('click', ()=>{
+    addCircle(1)
+})
+qTwo.addEventListener('click', ()=>{
+    addCircle(2)
+})
+qThree.addEventListener('click', ()=>{
+    addCircle(3)
+})
 qFour.addEventListener('click', ()=>{
-    addCircle(4)
+   addCircle(3)
+})
+
+
+function playAudio() {
+    myAudio.play()
+}
+
+
+bStart.addEventListener('click', ()=>{
+    let t = 60 / bpm * 1000
+    playFunction = setInterval(playAudio, t)
+})
+
+
+bStop.addEventListener('click', ()=>{
+    clearInterval(playFunction)
 })
